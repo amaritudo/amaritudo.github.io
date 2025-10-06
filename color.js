0e9b3df8ff79fd3c7a839698f9de664620823ea2
amaritudo <orin2021@mail.ru>
2025-10-06 17:22:51 +0700

    js

--- color.js
+++ color.js
@@ -3,4 +3,4 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelector("#red").addEventListener('click', function(){ body.style.backgroundColor="red";});
     document.querySelector("#green").addEventListener('click', function(){ body.style.backgroundColor="green";});
     document.querySelector("#blue").addEventListener('click', function(){ body.style.backgroundColor="blue";})
-}
+})
